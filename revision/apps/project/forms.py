8563011cb6a34adf71b61412bc7e7ed566324fea
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-09-26 15:57:16 +0200

    Added new project form

--- revision/apps/project/forms.py
+++ revision/apps/project/forms.py
@@ -3,7 +3,8 @@ from django import forms
 
 from .models import (Project, Video,)
 
-from crispy_forms.layout import Button, Field, Layout
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.layout import Button, Field, Layout, HTML, Fieldset, Div, ButtonHolder, Submit
 from parsley.decorators import parsleyfy
 
 
@@ -32,6 +33,30 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ('slug', 'collaborators', 'data',)
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.attrs = {
+            'id': 'create-project-form',
+            'parsley-validate': ''
+        }
+        self.helper.form_show_errors = False
+
+        self.helper.layout = Layout(
+            HTML('{% include "partials/form-errors.html" with form=form %}'),
+            Fieldset(
+                '',
+                Div(
+                    Field('name', css_class=''),
+                    css_class='form-name clearfix'
+                ),
+                Field('client'),
+            ),
+            ButtonHolder(
+                Submit('submit', 'Create')
+            )
+        )
+        super(ProjectForm, self).__init__(*args, **kwargs)
 
 
 @parsleyfy
