0d4fb71dbb939f73ed4723ad8d6da374a23cd1c5
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-09-28 20:11:31 +0200

    getting upload process working

--- revision/apps/project/static/js/videouploader.js
+++ revision/apps/project/static/js/videouploader.js
@@ -16,22 +16,44 @@ var VideoUploaderView = React.createClass({displayName: 'VideoUploaderView',
         var self = this;
         var config = this.props.uploader_config;
         var file = event.target.files[0];
+        var progress = $('div#progress');
+        var progress_bar = progress.find('div.progress-bar');
+        var progress_conversion = $('div#progress-conversion');
+        var progress_conversion_bar = progress_conversion.find('div.progress-bar');
 
         self.state.uploader.add({
             name: config.aws_path + file.name,
             file: file,
             progress: function ( progress ) {
-                // console.log('progress' + progress);
+                console.log('progress:' + progress);
                 var percent = Math.round(progress * 100, 2)
-                $('div#progress .progress-bar').width(percent+'%');
+                progress_bar.width(percent+'%');
+                progress_conversion.hide();
             },
-            complete: function () {
-console.log('complete');
+            complete: function ( data ) {
+                // post the new video event
+                var response = $(data.responseText);
+                var location = response.find('Location');
+                // video_url
+
+                var video_object = {
+                    name: file.name,
+                    video_url: location.text(),
+                    //video_type: file.type
+                };
+
+                VideoResource.create( video_object ).defer().done(function ( data ) {
+                    console.log(data)
+                    progress_bar.width('0%');
+
+                    progress_conversion_bar.width('0%');
+                    progress_conversion.show();
+
+                });
             },
         });
     },
     render: function () {
-        console.log(this.state.uploader)
         return (React.DOM.span(null, 
             React.DOM.span({className: "btn btn-success fileinput-button"}, 
                 React.DOM.i({className: "glyphicon glyphicon-plus"}), 
@@ -41,6 +63,9 @@ console.log('complete');
             React.DOM.br(null), 
             React.DOM.br(null), 
             React.DOM.div({id: "progress", className: "progress"}, 
+                React.DOM.div({className: "progress-bar progress-bar-success"})
+            ), 
+            React.DOM.div({id: "progress-conversion", className: "progress hide"}, 
                 React.DOM.div({className: "progress-bar progress-bar-success"})
             ), 
             React.DOM.div({id: "files", className: "files"})
--- revision/apps/project/static/js/videouploader.jsx
+++ revision/apps/project/static/js/videouploader.jsx
@@ -16,22 +16,44 @@ var VideoUploaderView = React.createClass({
         var self = this;
         var config = this.props.uploader_config;
         var file = event.target.files[0];
+        var progress = $('div#progress');
+        var progress_bar = progress.find('div.progress-bar');
+        var progress_conversion = $('div#progress-conversion');
+        var progress_conversion_bar = progress_conversion.find('div.progress-bar');
 
         self.state.uploader.add({
             name: config.aws_path + file.name,
             file: file,
             progress: function ( progress ) {
-                // console.log('progress' + progress);
+                console.log('progress:' + progress);
                 var percent = Math.round(progress * 100, 2)
-                $('div#progress .progress-bar').width(percent+'%');
+                progress_bar.width(percent+'%');
+                progress_conversion.hide();
             },
-            complete: function () {
-console.log('complete');
+            complete: function ( data ) {
+                // post the new video event
+                var response = $(data.responseText);
+                var location = response.find('Location');
+                // video_url
+
+                var video_object = {
+                    name: file.name,
+                    video_url: location.text(),
+                    //video_type: file.type
+                };
+
+                VideoResource.create( video_object ).defer().done(function ( data ) {
+                    console.log(data)
+                    progress_bar.width('0%');
+
+                    progress_conversion_bar.width('0%');
+                    progress_conversion.show();
+
+                });
             },
         });
     },
     render: function () {
-        console.log(this.state.uploader)
         return (<span>
             <span className="btn btn-success fileinput-button">
                 <i className="glyphicon glyphicon-plus"></i>
@@ -41,6 +63,9 @@ console.log('complete');
             <br/>
             <br/>
             <div id="progress" className="progress">
+                <div className="progress-bar progress-bar-success"></div>
+            </div>
+            <div id="progress-conversion" className="progress hide">
                 <div className="progress-bar progress-bar-success"></div>
             </div>
             <div id="files" className="files"></div>
